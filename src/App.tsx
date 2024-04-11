274249d9b320108f712adb71e7542a847d51ca3e
Christopher Dierkens <cjdierkens@gmail.com>
2024-04-11 15:12:58 -0400

    feat: things

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import "./App.css";
 import hero from "./assets/hero.jpg";
+import greenFlowerBackground from "./assets/green-flower-background.png";
+import greenFlowerBackgroundInverted from "./assets/green-flower-background-inverted.png";
 import { TbMushroomFilled } from "react-icons/tb";
+import { BsDot } from "react-icons/bs";
+
+import { PropsWithChildren } from "react";
 
 function TimelineIcon() {
-  return <TbMushroomFilled className="text-secondary text-3xl" />;
+  return <TbMushroomFilled className="text-secondary scale-150" />;
+}
+
+function TimelineLine() {
+  return <hr className="bg-secondary opacity-20" />;
+}
+
+function TimelineMiddle({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) {
+  return <div className={`timeline-middle px-1 ${className}`}>{children}</div>;
 }
 
 function App() {
@@ -12,126 +28,196 @@ function App() {
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage: `url(${hero})`,
+          background: `url(${greenFlowerBackgroundInverted}) left -10% / 500px no-repeat, url(${greenFlowerBackground}) right 110% / 500px no-repeat, url(${hero}) center / cover no-repeat`,
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay"></div>
+
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-            <button className="btn btn-accent">Get Started</button>
-            <button className="btn btn-secondary">Get Started</button>
-            <button className="btn btn-neutral">Get Started</button>
+          <div>
+            <h1 className="font-cursive mb-0 font-thin text-7xl">
+              Sara & Chris
+            </h1>
+            <div className="flex w-full text-sm font-serif justify-evenly content-center">
+              <time>Tuesday, August 6th</time>
+              <span className="flex content-start">⛰️</span>
+              Gorham, New Hampshire
+            </div>
           </div>
         </div>
       </div>
 
       <div className="container mx-auto p-4 text-base-content">
         <article className="prose mx-auto">
-          <h1>Garlic bread with cheese: What the science tells us</h1>
+          <h2>Accommodations</h2>
+
           <p>
-            For years parents have espoused the health benefits of eating garlic
-            bread with cheese to their children, with the food earning such an
-            iconic status in our culture that kids will often dress up as warm,
-            cheesy loaf for Halloween.
+            Available Sunday, August 4th 3:00 PM through Sunday, August 4th
+            11:00 PM for family and friends.
           </p>
-          <p>
-            But a recent study shows that the celebrated appetizer may be linked
-            to a series of rabies cases springing up around the country.
-          </p>
+
+          <ul>
+            <li>
+              <a href="https://maps.app.goo.gl/jp49Hm4UV4KWhT3X9">
+                19 Clay Brook Rd, Gorham, NH 03581
+              </a>
+            </li>
+            <li>
+              <a href="https://www.vrbo.com/249051ha?dateless=true">
+                VRBO Listing
+              </a>
+            </li>
+          </ul>
         </article>
 
         <div className="divider"></div>
 
+        <div className="prose mx-auto">
+          <h2>Timeline</h2>
+        </div>
+
         <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
           <li>
-            <div className="timeline-middle">
+            <TimelineLine />
+            <TimelineMiddle className="mt-3">
               <TimelineIcon />
+            </TimelineMiddle>
+
+            <div className="timeline-start md:text-end mt-2">
+              <h2 className="mt-0 text-3xl">Check-in</h2>
+              <p className="text-sm ml-2">Sunday, August 4th 3:00 PM</p>
             </div>
-            <div className="timeline-start md:text-end mb-10 pt-1.5">
-              <time className="font-mono italic">1984</time>
-              <div className="text-lg font-black">First Macintosh computer</div>
-              The Apple Macintosh—later rebranded as the Macintosh 128K—is the
-              original Apple Macintosh personal computer. It played a pivotal
-              role in establishing desktop publishing as a general office
-              function. The motherboard, a 9 in (23 cm) CRT monitor, and a
-              floppy drive were housed in a beige case with integrated carrying
-              handle; it came with a keyboard and single-button mouse.
-            </div>
-            <hr className="bg-secondary opacity-20" />
+
+            <TimelineLine />
           </li>
+
           <li>
-            <hr className="bg-secondary opacity-20" />
-            <div className="timeline-middle">
+            <TimelineLine />
+
+            <TimelineMiddle className="mt-2">
               <TimelineIcon />
+            </TimelineMiddle>
+
+            <div className="timeline-end mt-2">
+              <h2 className="mt-0 text-3xl">Free Time</h2>
+              <p className="text-sm">
+                Monday, August 5th - Tuesday, August 6th
+              </p>
             </div>
-            <div className="timeline-end mb-10 pt-1.5">
-              <time className="font-mono italic">1998</time>
-              <div className="text-lg font-black">iMac</div>
-              iMac is a family of all-in-one Mac desktop computers designed and
-              built by Apple Inc. It has been the primary part of Apple's
-              consumer desktop offerings since its debut in August 1998, and has
-              evolved through seven distinct forms
-            </div>
-            <hr className="bg-secondary opacity-20" />
+
+            <TimelineLine />
           </li>
+
           <li>
-            <hr className="bg-secondary opacity-20" />
-            <div className="timeline-middle">
+            <TimelineLine />
+            <TimelineMiddle className="mt-3">
               <TimelineIcon />
+            </TimelineMiddle>
+
+            <div className="timeline-start md:text-end mt-2">
+              <h2 className="mt-0 text-3xl">Wedding Day</h2>
+              <p className="text-sm ml-2">Tuesday, August 6th</p>
             </div>
-            <div className="timeline-start md:text-end mb-10 pt-1.5">
-              <time className="font-mono italic">2001</time>
-              <div className="text-lg font-black">iPod</div>
-              The iPod is a discontinued series of portable media players and
-              multi-purpose mobile devices designed and marketed by Apple Inc.
-              The first version was released on October 23, 2001, about 8+1⁄2
-              months after the Macintosh version of iTunes was released. Apple
-              sold an estimated 450 million iPod products as of 2022. Apple
-              discontinued the iPod product line on May 10, 2022. At over 20
-              years, the iPod brand is the oldest to be discontinued by Apple
-            </div>
-            <hr className="bg-secondary opacity-20" />
+
+            <TimelineLine />
           </li>
+
           <li>
-            <hr className="bg-secondary opacity-20" />
-            <div className="timeline-middle">
-              <TimelineIcon />
+            <TimelineLine />
+            <div className="timeline-middle px-1">
+              <BsDot className="text-secondary scale-150" />
             </div>
-            <div className="timeline-end mb-10 pt-1.5">
-              <time className="font-mono italic">2007</time>
-              <div className="text-lg font-black">iPhone</div>
-              iPhone is a line of smartphones produced by Apple Inc. that use
-              Apple's own iOS mobile operating system. The first-generation
-              iPhone was announced by then-Apple CEO Steve Jobs on January 9,
-              2007. Since then, Apple has annually released new iPhone models
-              and iOS updates. As of November 1, 2018, more than 2.2 billion
-              iPhones had been sold. As of 2022, the iPhone accounts for 15.6%
-              of global smartphone market share
+            <div className="timeline-start md:text-end">
+              <p className="indent-2 text-sm">1:00 PM Ceremony</p>
             </div>
-            <hr className="bg-secondary opacity-20" />
+            <TimelineLine />
           </li>
+
           <li>
-            <hr className="bg-secondary opacity-20" />
-            <div className="timeline-middle">
+            <TimelineLine />
+            <div className="timeline-middle px-1">
+              <BsDot className="text-secondary scale-150" />
+            </div>
+            <div className="timeline-start md:text-end">
+              <p className="indent-2 text-sm">1:00 PM Ceremony</p>
+            </div>
+            <TimelineLine />
+          </li>
+
+          <li>
+            <TimelineLine />
+
+            <TimelineMiddle className="mt-2">
               <TimelineIcon />
+            </TimelineMiddle>
+
+            <div className="timeline-end mt-2">
+              <h2 className="mt-0 text-3xl">Intimate Hike (Sara and Chris)</h2>
+              <p className="text-sm">Wednesday, August 7th</p>
             </div>
-            <div className="timeline-start md:text-end mb-10 pt-1.5">
-              <time className="font-mono italic">2015</time>
-              <div className="text-lg font-black">Apple Watch</div>
-              The Apple Watch is a line of smartwatches produced by Apple Inc.
-              It incorporates fitness tracking, health-oriented capabilities,
-              and wireless telecommunication, and integrates with iOS and other
-              Apple products and services
+
+            <TimelineLine />
+          </li>
+
+          <li>
+            <TimelineLine />
+            <div className="timeline-middle px-1">
+              <BsDot className="text-secondary scale-150" />
             </div>
+            <div className="timeline-end !self-end !my-0">
+              <p className="indent-2 text-sm">1:00 PM Ceremony</p>
+            </div>
+            <TimelineLine />
+          </li>
+
+          <li>
+            <TimelineLine />
+            <div className="timeline-middle px-1">
+              <BsDot className="text-secondary scale-150" />
+            </div>
+            <div className="timeline-end">
+              <p className="indent-2 text-sm">1:00 PM Ceremony</p>
+            </div>
+            <TimelineLine />
+          </li>
+
+          <li>
+            <TimelineLine />
+            <TimelineMiddle className="mt-3">
+              <TimelineIcon />
+            </TimelineMiddle>
+
+            <div className="timeline-start md:text-end mt-2">
+              <h2 className="mt-0 text-3xl">Check-out</h2>
+              <p className="text-sm ml-2">Sunday, August 4th 11:00 PM</p>
+            </div>
+
+            <TimelineLine />
           </li>
         </ul>
+
+        <div className="divider"></div>
+
+        <div className="prose mx-auto">
+          <h2>Vendors</h2>
+
+          <ul>
+            <li>
+              <a href="https://www.buckandbloom.com/">Buck and Bloom</a>
+            </li>
+            <li>
+              <a href="https://www.autumnnomad.com/weddings-home">
+                Autumn Nomad
+              </a>
+            </li>
+            <li>
+              <a href="https://apothecaflowershoppe.com/">Apotheca Flowers</a>
+            </li>
+            <li>
+              <a href="https://babel-photos.com/">Babel Photos</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cursive: [
+          "Ms Madi",
+          "Brush Script MT",
+          "Segoe Print",
+          "Chilanka",
+          "TSCu_Comic",
+          "casual",
+          "cursive",
+        ],
+      },
+    },
+  },
   daisyui: {
     themes: [
       {
@@ -10,7 +25,7 @@ export default {
           primary: "#133254",
           secondary: "#B5A280",
           accent: "#9CA18A",
-          neutral: "#C9BFAF",
+          neutral: "#DEEAF7",
           "base-100": "#ffffff",
           "base-content": "#133254",
           "neutral-content": "#133254",
